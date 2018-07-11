eec27583c67c63bd71a3b5f16b5b183b52e05a78
mayankmehtani <mayank.mehtani@gmail.com>
2018-07-11 11:10:03 -0700

    style changes

--- foobar1.py
+++ foobar1.py
@@ -1,20 +1,20 @@
 def answer(area):
     # your code here    
-    return_list = []
+    answer = []
     
     while (area != 0):
+        lowest_index=1 # lowest possible square root
+        highest_index=1000 # highest possible square root
 
-        lowest_index=1
-        highest_index=1000
-
+ 
         while ((lowest_index != highest_index) and (highest_index - 1 != lowest_index)):
             if area == highest_index**2:
-                return_list.append( ((highest_index))**2 )
-                return return_list
+                answer.append( ((highest_index))**2 )
+                return answer
             
             if area == lowest_index**2:
-                return_list.append( (lowest_index)**2)
-                return return_list
+                answer.append( (lowest_index)**2)
+                return answer
 
             if area < (( ( lowest_index + highest_index ) / 2 )**2):
                 highest_index = (( lowest_index + highest_index ) / 2 )
@@ -22,14 +22,14 @@ def answer(area):
                 lowest_index = ((lowest_index + highest_index)/2)
                             
         if lowest_index == highest_index:
-            return_list.append(lowest_index**2)
+            answer.append(lowest_index**2)
             area -= lowest_index**2
         else:
             if highest_index**2 <= area:
                 area -= highest_index**2
-                return_list.append(highest_index**2)
+                answer.append(highest_index**2)
             if lowest_index**2 <= area:
                 area -= lowest_index**2
-                return_list.append(lowest_index**2)
+                answer.append(lowest_index**2)
 
-    return return_list
+    return answer
